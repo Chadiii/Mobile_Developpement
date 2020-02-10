e8c2e1a9f5089fbebcfcc7b2f25d514902a90a7f
Chadiii <49269946+Chadiii@users.noreply.github.com>
2020-02-10 21:00:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tp1_devmobile/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.tp1_devmobile.Prevalent;
+
+import com.example.tp1_devmobile.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
