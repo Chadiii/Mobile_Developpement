48df533b50d5504993e683bce802a8d1bfc0682b
Chadiii <chadilaoulaou@gmail.com>
2020-03-11 17:30:28 +0100

    Adding Admin Order Manager

--- app/src/main/java/com/example/tp1_devmobile/AdminCategoryActivity.java
+++ app/src/main/java/com/example/tp1_devmobile/AdminCategoryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView tShits, sportsTShirts, femaleDresses, sweathers;
     private ImageView glasses, hatsCaps, walletsBagsPurses, shoes;
     private ImageView headPhonesHandFree, laptots, watches, mobilePhones;
+
+    private Button LogoutBtn, CheckOrders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +36,30 @@ public class AdminCategoryActivity extends AppCompatActivity {
         watches = (ImageView) findViewById(R.id.watches);
         mobilePhones = (ImageView) findViewById(R.id.mobilephones);
 
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        CheckOrders = (Button) findViewById(R.id.check_orders_btn);
 
 
 
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
 
+        CheckOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
 
         tShits.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/tp1_devmobile/AdminNewOrdersActivity.java
+++ app/src/main/java/com/example/tp1_devmobile/AdminNewOrdersActivity.java
@@ -1,14 +1,29 @@
 package com.example.tp1_devmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AdminNewOrdersActivity extends AppCompatActivity {
+
+    private RecyclerView ordersList;
+    private DatabaseReference ordersRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_new_orders);
+
+        ordersRef = FirebaseDatabase.getInstance().getReference()
+                .child("Orders");
+
+        ordersList = (RecyclerView) findViewById(R.id.orders_list);
+
+
     }
 }
